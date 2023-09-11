73819260bfae99dc4930bbe38f870f4fbe1fdc45
Timo Gühring <timo.guehring@googlemail.com>
2023-09-11 12:57:37 +0200

    Update

--- course/goal.py
+++ course/goal.py
@@ -10,24 +10,23 @@ DATA = "/Users/tguehring/Projects/private/python-excel-utilities/course/Region.x
 REGION_COL_IDX = 1
 CRE_ID_COL_IDX = 2
 
-SAMPLE_TEMPLATE = "/Users/tguehring/Projects/private/python-excel-utilities/excelsheets/Sheet1.xlsx"
 
-
-def copy_file_into_dir(source_file_path, target_location):
+def copy_file_into_dir(source_file_path: str, target_location: str) -> str:
     """
     To copy file into target directory
     """
-    shutil.copy(source_file_path, target_location)
+    return shutil.copy(source_file_path, target_location)
 
 
 def get_regions_from_excel_sheet(excel_sheet_path: str):
     workbook = openpyxl.load_workbook(excel_sheet_path)
     ws = workbook['Sheet1']
     max_row = ws.max_row
+    print(f"Max row: {max_row}")
     region_columns = []
     # Iterate through all the rows to get the regions
     # Start at row 2 to exclude the header 'Region'
-    for i in range(2, max_row):
+    for i in range(2, max_row + 1):
         region = ws.cell(row=i, column=REGION_COL_IDX).value
         print(f"Discovered region: {region}")
         region_columns.append(region)
@@ -35,33 +34,44 @@ def get_regions_from_excel_sheet(excel_sheet_path: str):
     return set(region_columns)
 
 
-def get_credids_from_excel_sheet(excel_sheet_path: str):
-    workbook = openpyxl.load_workbook(excel_sheet_path)
+def sanitize_excel_sheet(file_path: str, region: str):
+    """
+    To remove all rows that do not belong to the region
+    """
+    print("Sanitizing file: " + file_path)
+
+    workbook = openpyxl.load_workbook(file_path)
     ws = workbook['Sheet1']
     max_row = ws.max_row
-    creid_columns = []
-    # Iterate through all the rows to get the cre ids
-    # Start at row 3 to exclude the header 'CRE ID'
-    for i in range(3, max_row):
-        cre_id = ws.cell(row=i, column=CRE_ID_COL_IDX).value
-        print(f"Discovered CRE ID: {cre_id}")
-        creid_columns.append(cre_id)
-    # Create a set without duplicates
-    return set(creid_columns)
+    # Iterate through all the rows to get the regions
+    # Start at row 2 to exclude the header 'Region'
+    i = 2
+    while i <= max_row:
+        row_region = ws.cell(row=i, column=REGION_COL_IDX).value
+        print(f"Row {i} has region {row_region}")
+        if row_region is None or row_region == "":
+            break
+        elif row_region != region:
+            ws.delete_rows(i)
+        else:
+            i += 1
+
+    workbook.save(file_path)
 
 
-def setup_region_folders(regions):
+def setup_region_folders(regions, target_excel_sheet: str):
     """
-    To create folders for each region
+    To create folders for each region and copy the sanitized template file into it
     """
     for region in regions:
         dir_path = os.path.join(RESULT_LOCATION, region)
-        os.makedirs(dir_path)
+        os.makedirs(dir_path, exist_ok=True)
         print("Created folder: ", dir_path)
-        copy_file_into_dir(SAMPLE_TEMPLATE, dir_path)
+        new_region_file_path = copy_file_into_dir(target_excel_sheet, dir_path)
+        sanitize_excel_sheet(new_region_file_path, region)
 
 
 region_columns = get_regions_from_excel_sheet(DATA)
 print("Regions: " + str(region_columns))
 
-setup_region_folders(region_columns)
+setup_region_folders(region_columns, DATA)
