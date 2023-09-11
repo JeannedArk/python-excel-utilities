9756f412fa7f91b42f85912a9cc52d8ed6a72495
Timo Gühring <timo.guehring@googlemail.com>
2023-09-11 13:49:02 +0200

    Update

--- course/goal.py
+++ course/goal.py
@@ -2,11 +2,13 @@
 import os
 import shutil
 import openpyxl
+from collections import defaultdict
 
 
 RESULT_LOCATION = "/Users/tguehring/Downloads/testtesttest"
 
-DATA = "/Users/tguehring/Projects/private/python-excel-utilities/course/Region.xlsx"
+TEMPLATE_EXCEL_SHEET = "/Users/tguehring/Projects/private/python-excel-utilities/course/Region.xlsx"
+VALIDATION_EXCEL_SHEET = "/Users/tguehring/Projects/private/python-excel-utilities/course/Region.xlsx"
 REGION_COL_IDX = 1
 CRE_ID_COL_IDX = 2
 
@@ -18,20 +20,22 @@ def copy_file_into_dir(source_file_path: str, target_location: str) -> str:
     return shutil.copy(source_file_path, target_location)
 
 
-def get_regions_from_excel_sheet(excel_sheet_path: str):
+def get_regions_from_excel_sheet(excel_sheet_path: str) -> dict[str, list]:
     workbook = openpyxl.load_workbook(excel_sheet_path)
     ws = workbook['Sheet1']
     max_row = ws.max_row
     print(f"Max row: {max_row}")
-    region_columns = []
+    
+    region_creid_map = defaultdict(list)
     # Iterate through all the rows to get the regions
     # Start at row 2 to exclude the header 'Region'
     for i in range(2, max_row + 1):
         region = ws.cell(row=i, column=REGION_COL_IDX).value
-        print(f"Discovered region: {region}")
-        region_columns.append(region)
-    # Create a set without duplicates
-    return set(region_columns)
+        creid = ws.cell(row=i, column=CRE_ID_COL_IDX).value
+        print(f"Discovered region: {region} {creid}")
+        region_creid_map[region].append(creid)
+
+    return region_creid_map
 
 
 def sanitize_excel_sheet(file_path: str, region: str):
@@ -59,19 +63,18 @@ def sanitize_excel_sheet(file_path: str, region: str):
     workbook.save(file_path)
 
 
-def setup_region_folders(regions, target_excel_sheet: str):
+def setup_region_folders(region_creid_map, target_excel_sheet: str):
     """
-    To create folders for each region and copy the sanitized template file into it
+    To create folders for each region and copy the target_excel_sheet into it
     """
-    for region in regions:
-        dir_path = os.path.join(RESULT_LOCATION, region)
-        os.makedirs(dir_path, exist_ok=True)
-        print("Created folder: ", dir_path)
-        new_region_file_path = copy_file_into_dir(target_excel_sheet, dir_path)
-        sanitize_excel_sheet(new_region_file_path, region)
+    for region in region_creid_map:
+        region_dir_path = os.path.join(RESULT_LOCATION, region)
+        for creid in region_creid_map[region]:
+            creid_dir_path = os.path.join(region_dir_path, creid)
+            os.makedirs(creid_dir_path, exist_ok=True)
+            print("Created folder: ", creid_dir_path)
+            copy_file_into_dir(target_excel_sheet, creid_dir_path)
 
 
-region_columns = get_regions_from_excel_sheet(DATA)
-print("Regions: " + str(region_columns))
-
-setup_region_folders(region_columns, DATA)
+region_creid_map = get_regions_from_excel_sheet(TEMPLATE_EXCEL_SHEET)
+setup_region_folders(region_creid_map, VALIDATION_EXCEL_SHEET)
